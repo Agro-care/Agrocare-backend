20df5c696265afeb74af7fd4f8949590c7284501
kautilya <shivakumar.kautilya@gmail.com>
2024-09-26 20:30:07 -0400

    user authentication is completed successfully

---
+++ controllers/signup.js
@@ -0,0 +1,37 @@
+const user = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Emailvalidator = require("email-validator");
+exports.createUser = async(req, res) => {
+        try {
+            const { email, name, password } = req.body
+            console.log(email , name , password)
+            const checkUser = await user.findOne({ "email": email })
+            if (checkUser) {
+                res.status(404).json("email already available")
+            }
+            const validateEmail = Emailvalidator.validate(email)
+            if (!validateEmail) return res.status(401).json("Invalid Email");
+            const User = new user({
+                password: password,
+                email: email,
+                name
+            })
+            const saltrounds = 10
+            User.password = await bcrypt.hash(password, saltrounds)
+            User.save()
+            const payload = { userId: User._id }
+            const NewToken = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2d" })
+            res.json({
+                id: User._id,
+                username: User.username,
+                name: User.name,
+                token: NewToken,
+                verified: User.verified,
+                message: "Register Success ! please activate your email to start",
+                email: User.email
+            })
+        } catch (err) {
+            console.log(err)
+        }
+}
---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    ppImage: {
+        type: String,
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    Token: {},
+    resetToken: {},
+    expireToken: {},
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("user", userSchema)
